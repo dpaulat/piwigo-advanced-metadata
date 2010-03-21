3bba3ddb7853c9c9a79c3e495052263f44856e70
grum <grum@68402e56-0260-453c-a942-63ccdbb3a9ee>
2010-03-21 20:48:21 +0000

    fix bug when formating dates : invalid dates are now ignored
    git-svn-id: http://piwigo.org/svn/extensions/AMetaData@5229 68402e56-0260-453c-a942-63ccdbb3a9ee

--- JpegMetaData/Common/ConvertData.class.php
+++ JpegMetaData/Common/ConvertData.class.php
@@ -287,7 +287,11 @@
      */
     static public function toDateTime($dateTime)
     {
-      if(preg_match('/^(\d{4}:\d{2}:\d{2}\s\d{2}:\d{2}(:\d{2}){0,1}\x00{0,1}|\d{4}-\d{2}-\d{2}(T|\s)\d{2}:\d{2}(:\d{2}){0,1}(Z|[+|-]\d{2}:\d{2}){0,1}|\d{4}\d{2}\d{2})/', $dateTime))
+      if(preg_match('/^('.
+           '\d{4}:\d{2}:\d{2}\s\d{2}:\d{2}(:\d{2}){0,1}\x00{0,1}|'.
+           '\d{4}-\d{2}-\d{2}(T|\s)\d{2}:\d{2}(:\d{2}){0,1}(Z|[+|-]\d{2}:\d{2}){0,1}|'.
+           '\d{4}\d{2}\d{2}'.
+           ')$/', $dateTime))
       {
         $returned=new DateTime($dateTime, new DateTimeZone("UTC"));
       }
