a654a83ef7f081206836aa469ca961273e5aff81
TranslationTeam <TranslationTeam@68402e56-0260-453c-a942-63ccdbb3a9ee>
2013-05-08 11:51:55 +0000

    [advanced_metadata] Add et_EE, thanks to : Eggy
    git-svn-id: http://piwigo.org/svn/extensions/AMetaData@22554 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/et_EE/plugin.lang.php
@@ -0,0 +1,123 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['g003_metadata'] = 'Metainfo';
+$lang['g003_metadataRuleInvalid'] = 'Metadata reegel: metainfo peab olema valitud';
+$lang['g003_metadata_begin_all'] = 'Metainf o%s on olemas ja peab omistatama ühe järgmistest väärtustest:';
+$lang['g003_metadata_begin_one'] = 'Metainfo %s on olemas ja peab algama järgnevaga:';
+$lang['g003_metadata_detail'] = 'Metainfo võimalikud väärtused';
+$lang['g003_metadata_dont_exists'] = 'Metainfo %s ei ole olemas.';
+$lang['g003_metadata_end_all'] = 'Metainfo %s on olemas ja peab lõppema ühe väärtustega järgnevatest:';
+$lang['g003_metadata_end_one'] = 'Metainfo %s on olemas ja peab lõppema järgnevaga:';
+$lang['g003_metadata_equals_all'] = 'Metainfo %s on olemas ja on võrdne ühega järgnevatest_';
+$lang['g003_metadata_equals_one'] = 'Metainfo %s on olemas ja on võrdne järgnevaga:';
+$lang['g003_metadata_exists'] = 'Metainfo %s on olemas';
+$lang['g003_metadata_like_all'] = 'Metainfo %s on olemas ja peab sisaldama ühte väärtust järgnevatest:';
+$lang['g003_metadata_like_one'] = 'Metainfo %s on olemas ja peab sisaldama:';
+$lang['g003_metadata_not_begin_all'] = 'Metainfo %s on olemas ja peab algama ühe väärtusega järgnevatest:';
+$lang['g003_help_tab_exif'] = 'EXIF';
+$lang['g003_help_tab_iptc'] = 'IPTC';
+$lang['g003_help_tab_magic'] = 'Magic';
+$lang['g003_help_tab_xmp'] = 'XMP';
+$lang['g003_ignoreMetadata'] = 'Ignoreeri järgmist metainfot:';
+$lang['g003_informations'] = 'Informatsioon';
+$lang['g003_install'] = 'Installatsioon';
+$lang['g003_invalidId'] = 'Metainfo identifikaator ei ole sobiv';
+$lang['g003_invalid_group_id'] = 'Vale metainfo grupi ID';
+$lang['g003_invalid_random_number'] = 'Töödeldavate piltide kogus ei ole sobiv';
+$lang['g003_keyword'] = 'Võtmesõnad';
+$lang['g003_loading'] = 'Laadimine...';
+$lang['g003_magic_filter'] = 'Magic(arvutatud metainfo)';
+$lang['g003_metadatId'] = 'metainfo identifikaator';
+$lang['g003_gpc_not_up_to_date'] = 'Lisa jaoks on vajalik lisa <i>Grum Plugin Classes</i> installeerimine, vähemalt versioon %s
+Praegune versioon %s : palun uuenda pluginat <i>Grum Plugin Classes</i>.';
+$lang['g003_databaseWarning2_1'] = '[p]Viimasest plugini uuendusest alates on olemas uus metainfo :[/p][ul]%s[/ul]
+[p]
+Info kasutamiseks on vajalik hoidla uuendamine.[/p]';
+$lang['g003_databaseWarning2_n'] = '[p]Viimasest plugini uuendusest alates on olemas mõningane uus metainfo:[/p][ul]%s[/ul]
+[p]
+Info kasutamiseks on vajalik hoidla uuendamine.[/p]';
+$lang['g003_database_is_not_up_to_date'] = 'Hoidla ei ole ajakohane!';
+$lang['g003_default_group_name'] = 'Pildistamise olud';
+$lang['g003_delete'] = 'Kustuta';
+$lang['g003_deleteMetadata'] = 'Metainfo kustutamine';
+$lang['g003_deleting_a_group'] = 'Metainfo grupi eemaldamine';
+$lang['g003_display'] = 'Kuva';
+$lang['g003_display_management'] = 'Metainfo kuvamise haldamine';
+$lang['g003_editing_a_group'] = 'Metainfo grupi redigeerimine';
+$lang['g003_error_invalid_ajax_call'] = 'Vigane kutse funktsioon!';
+$lang['g003_exclude_unused_tags'] = 'Jäta kasutama metainfo välja';
+$lang['g003_fillDatabaseContinuously'] = 'Täida hoidlat jätkuvalt';
+$lang['g003_fillDatabaseIgnoreWarning'] = 'Selle grupi parameetrite muutmise rakendamine vajab uut analüüsi';
+$lang['g003_filter'] = 'Filter';
+$lang['g003_help'] = 'Metainfo abi';
+$lang['g003_conditionMdRuleInvalid'] = '"Tingimuslik" reegel: metainfo peab olema valitud';
+$lang['g003_conditionRulesRuleInvalid'] = '"Tingimuslik" reegel: peab olema vähemalt 1 reegel';
+$lang['g003_confirm_group_delete'] = 'Oled sa kindel, et tahad kustutada grupi %s metainfo?';
+$lang['g003_convert_keywords_and_apply'] = 'Konverteeri';
+$lang['g003_convert_ok'] = 'Konverteerimine oli edukas';
+$lang['g003_database'] = 'Hoidla';
+$lang['g003_databaseInformation'] = 'Oluline informatsioon hoidla kohta';
+$lang['g003_basic_mode_help'] = '[i]Lihtne[/i] režiim on neile, kes tahavad ainult põhilist infot kuvada:[ul]
+[li]võimalikult lihtne kasutajaliides[/li]
+[li]lühike metainfo nimekiri (140 märget)[/li]
+[/ul]';
+$lang['g003_analyze_caddie_add_pictures'] = 'Analüüs keskendub korvis olevatele piltidele ja lisab selle olemasolevasse hoidlasse';
+$lang['g003_analyze_caddie_replace_pictures'] = 'Analüüs keskendub korvis olevatele piltidele ja asendab olemasoleva hoidla sisu sellega';
+$lang['g003_analyze_in_progress'] = 'Analüüs kestab...';
+$lang['g003_analyze_is_finished'] = 'Analüüs lõpetatud';
+$lang['g003_analyze_not_analyzed_pictures'] = 'Analüüs keskendub mitte kunagi analüüsitud piltidele ja lisab selle olemasolevasse hoidlasse';
+$lang['g003_analyze_random_pictures'] = 'Analüüs keskendub %s juhuslikult valitud pildile mitte kunagi analüüsitud piltide seast ja lisab selle olemasolevasse hoidlasse';
+$lang['g003_basic_mode'] = 'Lihtne';
+$lang['g003_cancel'] = 'Katkesta';
+$lang['g003_choose_a_metadata'] = 'Teosta otsin metainfo väärtuse järgi';
+$lang['g003_click_to_delete_group'] = 'Klõpsa, et eemaldada metaifo grupp';
+$lang['g003_click_to_edit_group'] = 'Klõpsa metainfo grupi valikute redigeerimiseks';
+$lang['g003_click_to_manage_group'] = 'Klõpsa metainfo grupi elementide manageerimiseks';
+$lang['g003_click_to_manage_list'] = 'Klõpsa metainfo lisamiseks/eemaldamiseks';
+$lang['g003_conditionIf'] = 'Testi kas metainfo';
+$lang['g003_advanced_mode_help'] = '[i]Edasijõudnute[/i] režiim on neile, kes soovivad maksimaalselt metainfot kasutada:[ul]
+[li]keerulisem ja kõikide võimalustega kasutajaliides[/li]
+[li]täielik metainfo nimekiri (umbes 540)[/li]
+[li]laiendatud funktsionaalsus (otsing...)[/li]
+[/ul]
+[i]Edasijõudnute[/i] režiim nõuab hoidla tegemist.';
+$lang['Grum Plugin Classes is not installed'] = '<b>Grum Plugin Classes</b> ei ole installeeritud';
+$lang['amd_title_page'] = 'Metainfo edasijõudnud haldamine';
+$lang['g003_1_picture_in_caddie'] = '1 pilt korvis';
+$lang['g003_NumOfImage'] = 'pildid';
+$lang['g003_Pct'] = '%';
+$lang['g003_TagId'] = 'Metainfo';
+$lang['g003_TagLabel'] = 'Nimi';
+$lang['g003_Value'] = 'Väärtus';
+$lang['g003_add'] = 'Lisa';
+$lang['g003_add_a_group'] = 'Lisa metainfo grupp';
+$lang['g003_add_a_new_md'] = 'Lisa uus metainfo';
+$lang['g003_add_a_rule'] = 'Lisa reegel';
+$lang['g003_add_delete_tags'] = 'Lisa/eemalda metainfo';
+$lang['g003_add_metadata'] = 'Lisa metainfo';
+$lang['g003_adding_a_group'] = 'Metainfo grupi lisamine';
+$lang['g003_advanced_mode'] = 'Edasijõudnud';
+$lang['g003_analyze'] = 'Analüüs';
+$lang['g003_analyze_all_pictures'] = 'Analüüs sisaldab kõiki galeriis olevaid pilte ja asendab nede praeguses hoidlas';
+$lang['g003_analyze_analyzed_pictures'] = 'Analüüs sisaldab ainult juba analüüsitud pilte';
+?>
