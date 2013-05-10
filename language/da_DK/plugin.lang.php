d21d1323ff0186cf3654e716c74141ae22769890
TranslationTeam <TranslationTeam@68402e56-0260-453c-a942-63ccdbb3a9ee>
2013-05-10 12:55:53 +0000

    [advanced_metadata] Add da_DK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/extensions/AMetaData@22586 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,230 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['g003_typeCondition'] = 'Tilstand';
+$lang['g003_typeMetadata'] = 'Metadata';
+$lang['g003_typeText'] = 'Tekst';
+$lang['g003_update'] = 'Opdater';
+$lang['g003_update_metadata'] = 'Opdater metadataarkiv';
+$lang['g003_updating_metadata'] = 'Arkivopdatering';
+$lang['g003_userDefined_filter'] = 'Tilpassede metadata';
+$lang['g003_validate'] = 'OK';
+$lang['g003_valueOrder'] = 'Værdi';
+$lang['g003_value_already_set'] = 'Værdien er allerede defineret i værdidomænet';
+$lang['g003_version'] = 'v';
+$lang['g003_warning_on_analyze_0'] = 'Advarsel!';
+$lang['g003_warning_on_analyze_1'] = 'Opbygning af arkivet ved hjælp af direkte analyse-processen, kan tage lang tid (op til flere minutters behandlingstid) og være ressourcekrævende for serveren, afhængigt af antallet af fotografier udvalgt til analysering.';
+$lang['g003_warning_on_analyze_2'] = 'Denne form for brug kan være forbudt hos nogle værter.';
+$lang['g003_warning_on_analyze_3'] = 'Arkivet modtager gradvist data, hver gang i side i galleriet besøges. Dermed er den tid det vil tage, at gøre det komplet, afhængig af:';
+$lang['g003_warning_on_analyze_3a'] = 'antallet af billeder i galleriet';
+$lang['g003_warning_on_analyze_3b'] = 'antallet af billeder der vises dagligt';
+$lang['g003_warning_on_analyze_5'] = 'For hurtigt at få et komplet arkiv, er en mere komplet analyse af galleriet mulig:';
+$lang['g003_y'] = 'Ja';
+$lang['g003_yes'] = 'Ja';
+$lang['g003_state'] = 'Tilstand';
+$lang['g003_status'] = 'Status';
+$lang['g003_status_of_database'] = 'Arkivstatus';
+$lang['g003_step_1'] = 'Vælg hvordan plugin\'en skal benyttes';
+$lang['g003_tagIdAlreadyExist'] = 'Metadata med denne identifikation findes allerede!';
+$lang['g003_tagOrder'] = 'Metadata';
+$lang['g003_tag_in_piwigo'] = 'Tilstede i Piwigo';
+$lang['g003_tags'] = 'Tags';
+$lang['g003_tags_page_help'] = 'Denne funktion udfører et udtræk af nøgleord, som er til stede i metadatene vedrørende dine fotografier, og gør det muligt at konvertere [i]Tags[/i].
+Billederne til hvilke nøgleord allerede er konverteret og tilknyttet, vises ikke: der foreslås kun nøgleord hørende til billeder, til hvilke en konvertering og mulig tilknytning er mulig.';
+$lang['g003_textRuleInvalid'] = 'Regel af typen "Tekst": teksten må ikke være tom';
+$lang['g003_typeCIfBeginWith'] = 'begynd med';
+$lang['g003_typeCIfEndWith'] = 'slut med';
+$lang['g003_typeCIfEqual'] = 'lig med';
+$lang['g003_typeCIfExist'] = 'eksisterer';
+$lang['g003_typeCIfLike'] = 'indeholder';
+$lang['g003_typeCIfNotBeginWith'] = 'begynder ikke med';
+$lang['g003_typeCIfNotEndWith'] = 'slutter ikke med';
+$lang['g003_typeCIfNotEqual'] = 'er ikke lig med';
+$lang['g003_typeCIfNotExist'] = 'eksisterer ikke';
+$lang['g003_typeCIfNotLike'] = 'indeholder ikke';
+$lang['g003_number_of_keywords'] = 'Antal nøgleord:';
+$lang['g003_ok'] = 'Ok';
+$lang['g003_oneRuleIsNeeded'] = 'Der skal være mindst én regel';
+$lang['g003_options'] = 'Valgmuligheder';
+$lang['g003_order'] = 'Sortér efter';
+$lang['g003_personnal'] = 'Tilpas';
+$lang['g003_personnal_metadata'] = 'Tilpas metadata';
+$lang['g003_personnal_page_help'] = 'Man kan let opbygge sine egne metadata fra eksisterende metadata.
+[ul]
+[li]Tilføj nyt metadata[/li]
+[li]Udfyld egenskaberne[/li]
+[li]Tilføj regler og kombinér dem om nødvendig med træk og slip[/li]
+[/ul]';
+$lang['g003_pleaseConfirmMetadataDelete'] = 'Skal metadata slettes?';
+$lang['g003_please_set_a_value'] = 'Opsæt en værdi';
+$lang['g003_rules'] = 'Regler';
+$lang['g003_search'] = 'Søg';
+$lang['g003_search_page_help'] = 'Det er muligt at udføre forskellige søgninger på metadataindholdet, fra simple til komplekse: tilføj kriterier og kombinér dem med træk og slip.';
+$lang['g003_select'] = 'Udvælgelse';
+$lang['g003_select_metadata'] = 'Udvælgelse af metadata';
+$lang['g003_select_page_help'] = 'Kun metadata udvalgt her er tilgængelige i andre grænsefladers opsætning: dermed kan metadatalisten begrænses til dem, der lader til at være mest relevante for dig.
+Udvælgelsen gennemføres med det samme (det er ikke nødvendig at godkende).';
+$lang['g003_selected_tag_isnot_linked_with_any_picture'] = 'De valgte metadata er ikke forbundet med nogen billeder';
+$lang['g003_selected_tags_only'] = 'Returnér kun valgte metadata';
+$lang['g003_show'] = 'Gennemse';
+$lang['g003_sizeAndRows'] = 'Arkivets vægt er %s og det indeholder %s metadata';
+$lang['g003_metadata_value_check_all'] = 'Alle værdierne skal være sande:';
+$lang['g003_metadata_value_check_one'] = 'Mindst en af de følgende skal være sande';
+$lang['g003_n'] = 'Nej';
+$lang['g003_n_pictures_in_caddie'] = '%s billeder i kurven';
+$lang['g003_name'] = 'Navn';
+$lang['g003_new_group'] = 'Ny metadatagruppe';
+$lang['g003_no'] = 'Nej';
+$lang['g003_no_filter'] = 'Intet filter';
+$lang['g003_no_items_selected'] = 'Ingen metadata er valgt';
+$lang['g003_no_keywords'] = 'Der blev ikke fundet nøgleord, der kan konverteres';
+$lang['g003_no_tag_can_be_selected'] = 'Ingen metadata er tilgængelige';
+$lang['g003_numOrder'] = 'Billednummer';
+$lang['g003_num_of_pictures'] = 'Antal fotografier';
+$lang['g003_num_of_pictures_already_tagged'] = 'Antal allerede taggede fotografier';
+$lang['g003_num_of_rules'] = 'Antal regler';
+$lang['g003_numberOfAnalyzedPictures'] = '%s billeder blev analyseret';
+$lang['g003_numberOfNotAnalyzedPictures'] = '%s billeder er ikke blevet analyseret';
+$lang['g003_numberOfPicturesWithoutTags'] = '%s billeder har ikke metadata';
+$lang['g003_number_of_distinct_values'] = 'Antal særskiltet værdier:';
+$lang['g003_number_of_filtered_metadata'] = 'Metadatanummer:';
+$lang['g003_metadata_begin_all'] = 'Metadata %s er til stede og skal begynde med enhver af følgende værdier:';
+$lang['g003_metadata_begin_one'] = 'Metadata %s er til stede og skal begynde med:';
+$lang['g003_metadata_detail'] = 'Mulige metadataværdier';
+$lang['g003_metadata_dont_exists'] = 'Metadata %s er ikke til stede';
+$lang['g003_metadata_end_all'] = 'Metadata %s er til stede skal slutte med enhver af følgende værdier:';
+$lang['g003_metadata_end_one'] = 'Metadata %s er til stede og skal slutte med:';
+$lang['g003_metadata_equals_all'] = 'Metadata %s er til stede og er lig med en af følgende værdier:';
+$lang['g003_metadata_equals_one'] = 'Metadata %s er til stede og er lig med:';
+$lang['g003_metadata_exists'] = 'Metadata %s er til stede';
+$lang['g003_metadata_like_all'] = 'Metadata %s er til stede og skal indeholder en af følgende værdier:';
+$lang['g003_metadata_like_one'] = 'Metadata %s er til stede og skal indeholde:';
+$lang['g003_metadata_not_begin_all'] = 'Metadata %s er til stede og må ikke begynde med en af følgende værdier:';
+$lang['g003_metadata_not_begin_one'] = 'Metadata %s er til stede og må ikke begynde med:';
+$lang['g003_metadata_not_end_all'] = 'Metadata %s er til stede og må ikke slutte med en af følgende værdier:';
+$lang['g003_metadata_not_end_one'] = 'Metadata %s er til stede og må ikke slutte med:';
+$lang['g003_metadata_not_equals_all'] = 'Metadata %s er til stede og må ikke være lig med en af følgende værdier:';
+$lang['g003_metadata_not_equals_one'] = 'Metadata %s er til stede og må ikke være lig med:';
+$lang['g003_metadata_not_like_all'] = 'Metadata %s er til stede og må ikke indeholde nogen af følgende værdier:';
+$lang['g003_metadata_not_like_one'] = 'Metadata %s er til stede og må ikke indeholde:';
+$lang['g003_help_tab_magic'] = 'Magi';
+$lang['g003_display_page_help'] = 'Metadata gemt sammen med billedet kan sortere og grupperes.  Som standard er kun gruppen [i]Optagelsestilstand[/i] tilgængelig, men det er muligt at oprette så mange, som der er brug for ([i]IPTC[/i], [i]Geolokation[/i], ...).
+[ul]
+[li]Opret metadatagrupper efter behov[/li]
+[li]Tilføj de metadata, der skal vises[/li]
+[li]Inden for en gruppe ændres visningsrækkefølgen ved hjælp af træk og slip[/li]
+[li]Ændring af gruppers visningsrækkefølge sker ved hjælp af træk og slip[/li]
+[/ul]
+Udvælgelse og sortering træder i kraft omgående (det er ikke nødvendigt at godkende).';
+$lang['g003_fillDatabaseIgnoreWarning'] = 'Skal udføres, ændring af parametrene på denne handling kræver en ny analyse';
+$lang['g003_gpc_not_up_to_date'] = 'Plugin\'en kræver at <i>Grum Plugin Classes</i> version %s er installeret.
+I øjeblikket er version %s installeret: vær venlig at opdatere <i>Grum Plugin Classes</i> til den krævede version.';
+$lang['g003_advanced_mode_help'] = 'Den [i]avancerede[/i] tilstand er beregnet til dem, der ønsker at få det maksimale ud af hvad deres fotografier og metadata. Den tilbyder:[ul]
+[li]en mere kompleks, men komplet, grænseflade[/li]
+[li]en komplet liste over metadata (cirka 540)[/li]
+[li]udvidet funktionalitet (statistik, søgning ...)[/li]
+[/ul]
+Den [i]avancerede[/i] tilstand er nødvendig for at kunne opbygge et arkiv.';
+$lang['g003_help'] = 'Hjælp til metadata';
+$lang['g003_ignoreMetadata'] = 'Ignorer følgende metadata';
+$lang['g003_informations'] = 'Oplysninger';
+$lang['g003_install'] = 'Installering';
+$lang['g003_invalidId'] = 'Metadataidentifikationen er ikke gyldig';
+$lang['g003_invalid_group_id'] = 'Ugyldig metadatagruppeidentifikation';
+$lang['g003_invalid_random_number'] = 'Antallet af billeder, der skal behandles, er ikke gyldigt';
+$lang['g003_keyword'] = 'Nøgleord';
+$lang['g003_loading'] = 'Indlæser ...';
+$lang['g003_magic_filter'] = 'Magisk (beregnede metadata)';
+$lang['g003_metadatId'] = 'Metadataidenfikation';
+$lang['g003_metadata'] = 'Metadata';
+$lang['g003_metadataRuleInvalid'] = 'Regel af typen "Metadata": metadata skal være valgt';
+$lang['g003_help_tab_exif'] = 'Exif';
+$lang['g003_help_tab_iptc'] = 'IPTC';
+$lang['g003_help_tab_xmp'] = 'XMP';
+$lang['g003_database'] = 'Arkiv';
+$lang['g003_databaseInformation'] = 'Vigtige oplysninger om arkivet';
+$lang['g003_databaseWarning1'] = '[p]Arkivet er opbygget ved hjælp af metadata indeholdt i billeder fra galleriet, såvel som metadata beregnet af plugin\'en.  Afhængigt af antallet af billeder og antallet af metadata der skal tilknyttes, kan arkivet blive meget stort.  Før man begynder at anvende arkivet, skal man sikre sig at databasen der stilles til rådighed af ens vært, tillader en sådan brug.[/p]
+[p]Det er dog valgfrit, om man vil benytte arkivet, da det ikke anvendes til at vise metadata vedrørende fotografier i galleriet.[/p]
+[p]Det er nødvendigt at have et arkiv med data, hvis man ønsker:[/p]
+[ul]
+[li]Statistikker på metadataene i ens fotografier (det hjælper også med udvælgelsen af metadata)[/li]
+[li]Benytte søgemaskinen[/li]
+[/ul]';
+$lang['g003_databaseWarning2_1'] = '[p]Nyt metadata er blevet tilgængeligt siden den seneste opdatering af plugin\'en:[/p][ul]%s[/ul]
+[p]For at udnytte det skal arkivet opdateres.[/p]';
+$lang['g003_databaseWarning2_n'] = '[p]Nogle nye metadata er blevet tilgængelige siden den seneste opdatering af plugin\'en:[/p][ul]%s[/ul]
+[p]For at udnytte dem, skal arkivet opdateres.[/p]';
+$lang['g003_database_is_not_up_to_date'] = 'Arkivet er ikke opdateret!';
+$lang['g003_default_group_name'] = 'Optagelsestilstand';
+$lang['g003_delete'] = 'Slet';
+$lang['g003_deleteMetadata'] = 'Sletter metadataene';
+$lang['g003_deleting_a_group'] = 'Fjerner en metadatagruppe';
+$lang['g003_display'] = 'Vis';
+$lang['g003_display_management'] = 'Håndtering af visning af metadata';
+$lang['g003_editing_a_group'] = 'Redigering af en metadatagruppe';
+$lang['g003_error_invalid_ajax_call'] = 'Ugyldigt funktionskald!';
+$lang['g003_exclude_unused_tags'] = 'Fravælg ikke-benyttede metadata';
+$lang['g003_fillDatabaseContinuously'] = 'Opdatér arkivet løbende';
+$lang['g003_filter'] = 'Filtrer';
+$lang['g003_analyze_caddie_add_pictures'] = 'Analysen fokuserer på billederne i kurven, og føjer dem til det eksisterende arkiv';
+$lang['g003_analyze_caddie_replace_pictures'] = 'Analysen fokuserer på billederne i kurven, og erstatter det nuværende arkiv';
+$lang['g003_analyze_in_progress'] = 'Analyse i gang ...';
+$lang['g003_analyze_is_finished'] = 'Analyse gennemført';
+$lang['g003_analyze_not_analyzed_pictures'] = 'Analysen fokuserer på billeder, der aldrig før har været analyseret, og føjer dem til det eksisterende arkiv';
+$lang['g003_analyze_random_pictures'] = 'Analysen fokuserer på %s tilfældigt valgte billeder, blandt dem der aldrig før har været analyseret, og tilføjer dem til det eksisterende arkiv';
+$lang['g003_basic_mode'] = 'Grundlæggende';
+$lang['g003_basic_mode_help'] = 'Den [i]grundlæggende[/i] tilstand er til dem, der blot ønsker at vise metadata vedrørende deres fotografier, og den tilbyder:[ul]
+[li]en grænseflade, der er så simpel som muligt[/li]
+[li]en kort liste over metadata (cirka 140, de meste almindelige)[/li]
+[/ul]';
+$lang['g003_cancel'] = 'Annuller';
+$lang['g003_choose_a_metadata'] = 'Udfør søgning på metadataværdier';
+$lang['g003_click_to_delete_group'] = 'Klik for at fjerne metadatagruppen';
+$lang['g003_click_to_edit_group'] = 'Klik for at redigere egenskaber ved metadatagruppen';
+$lang['g003_click_to_manage_group'] = 'Klik for at håndtere elementer i metadatagruppen';
+$lang['g003_click_to_manage_list'] = 'Klik for at tilføje/fjerne metadata';
+$lang['g003_conditionIf'] = 'Afprøv om metadataene';
+$lang['g003_conditionMdRuleInvalid'] = 'Regel af typen "Betingelse": metadata skal være valgt';
+$lang['g003_conditionRulesRuleInvalid'] = 'Regel af typen "Betingelse": der skal være mindst én regel';
+$lang['g003_confirm_group_delete'] = 'Er du sikker på, at du ønsker at slette metadatagruppen %s?';
+$lang['g003_convert_keywords_and_apply'] = 'Konverter';
+$lang['g003_convert_ok'] = 'Konverteringen blev udført korrekt';
+$lang['Grum Plugin Classes is not installed'] = 'Plugin\'en <b>Grum Plugin Classes</b> er ikke installeret';
+$lang['amd_title_page'] = 'Håndtering af Advanced Metadata';
+$lang['g003_1_picture_in_caddie'] = '1 billede i kurv';
+$lang['g003_NumOfImage'] = 'Billeder';
+$lang['g003_Pct'] = '%';
+$lang['g003_TagId'] = 'Metadata';
+$lang['g003_TagLabel'] = 'Navn';
+$lang['g003_Value'] = 'Værdi';
+$lang['g003_add'] = 'Tilføj';
+$lang['g003_add_a_group'] = 'Tilføj en metadatagruppe';
+$lang['g003_add_a_new_md'] = 'Tilføj nye metadata';
+$lang['g003_add_a_rule'] = 'Tilføj en regel';
+$lang['g003_add_delete_tags'] = 'Tilføj/fjern metadata';
+$lang['g003_add_metadata'] = 'Tilføj metadata';
+$lang['g003_adding_a_group'] = 'Tilføjer en metadatagruppe';
+$lang['g003_advanced_mode'] = 'Avanceret';
+$lang['g003_analyze'] = 'Analyser';
+$lang['g003_analyze_all_pictures'] = 'Analysen indeholder alle billeder i galleriet, og erstatter det nuværende arkiv';
+$lang['g003_analyze_analyzed_pictures'] = 'Analysen omfatter kun de billeder, som allerede er blevet analyseret';
+?>
