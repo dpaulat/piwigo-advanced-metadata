c408940317cec28a20579c7654e56c6f0e5cef9e
TranslationTeam <TranslationTeam@68402e56-0260-453c-a942-63ccdbb3a9ee>
2013-06-02 05:09:02 +0000

    [advanced_metadata] Update nl_NL, thanks to : Kees Hessels
    git-svn-id: http://piwigo.org/svn/extensions/AMetaData@23009 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,78 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['g003_metadata_dont_exists'] = 'Metadata %s niet aanwezig';
+$lang['g003_metadata_equals_one'] = 'Metadata %s is aanwezig en is gelijk aan:';
+$lang['g003_metadata_exists'] = 'Metadata %s is aanwezig';
+$lang['g003_keyword'] = 'sleutelwoord';
+$lang['g003_loading'] = 'Laden...';
+$lang['g003_metadata'] = 'Metadata';
+$lang['g003_n'] = 'Nee';
+$lang['g003_name'] = 'Naam';
+$lang['g003_no'] = 'Nee';
+$lang['g003_no_items_selected'] = 'Geen metadata geselecteerd';
+$lang['g003_num_of_rules'] = 'Aantal regels';
+$lang['g003_typeText'] = 'Tekst';
+$lang['g003_tags'] = 'Labels';
+$lang['g003_typeCIfBeginWith'] = 'begint met';
+$lang['g003_typeCIfEndWith'] = 'eindigt met';
+$lang['g003_typeCIfEqual'] = 'is gelijk aan';
+$lang['g003_typeCIfExist'] = 'bestaat';
+$lang['g003_typeCIfLike'] = 'bevat';
+$lang['g003_typeCIfNotBeginWith'] = 'begint niet met';
+$lang['g003_typeCIfNotEndWith'] = 'eindigt niet met';
+$lang['g003_typeCIfNotEqual'] = 'is niet gelijk aan';
+$lang['g003_typeCIfNotExist'] = 'bestaat niet';
+$lang['g003_typeCIfNotLike'] = 'bevat niet';
+$lang['g003_typeMetadata'] = 'Metadata';
+$lang['g003_validate'] = 'OK';
+$lang['g003_valueOrder'] = 'Waarde';
+$lang['g003_warning_on_analyze_0'] = 'Waarschuwing!';
+$lang['g003_y'] = 'Ja';
+$lang['g003_yes'] = 'Ja';
+$lang['g003_numberOfPicturesWithoutTags'] = '%d afbeeldingen hebben geen metadata';
+$lang['g003_number_of_keywords'] = 'Aantal sleutelwoorden';
+$lang['g003_ok'] = 'Ok';
+$lang['g003_options'] = 'Opties';
+$lang['g003_order'] = 'Gesorteerd op';
+$lang['g003_rules'] = 'Regels';
+$lang['g003_search'] = 'Zoek';
+$lang['g003_select'] = 'Selectie';
+$lang['g003_select_metadata'] = 'Metadata selectie';
+$lang['g003_new_group'] = 'Nieuwe metadata-groep';
+$lang['g003_no_filter'] = 'Geen filter';
+$lang['g003_no_tag_can_be_selected'] = 'Geen metadata beschikbsaar';
+$lang['g003_num_of_pictures'] = 'Aantal afbeeldingen';
+$lang['g003_NumOfImage'] = 'afbeeldingen';
+$lang['g003_Pct'] = '%';
+$lang['g003_TagId'] = 'Metadata';
+$lang['g003_TagLabel'] = 'Naam';
+$lang['g003_Value'] = 'Waarde';
+$lang['g003_add'] = 'Toevoegen';
+$lang['g003_add_a_group'] = 'Voeg een groep metadata toe';
+$lang['g003_add_a_new_md'] = 'Voeg een nieuwe metadata toe';
+$lang['g003_add_a_rule'] = 'Voeg een nieuwe regel toe';
+$lang['g003_add_delete_tags'] = 'Toevoegen/Verwijderen metadata';
+$lang['g003_add_metadata'] = 'Voeg een metadata toe';
+$lang['g003_adding_a_group'] = 'Een groep metadata wordt toegevoegd';
+$lang['g003_analyze'] = 'Analyzeer';
+?>
