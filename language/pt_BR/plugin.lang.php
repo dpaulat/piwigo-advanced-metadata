5b567e1d6ba87cbe819396bcac370d3045e91ff0
TranslationTeam <TranslationTeam@68402e56-0260-453c-a942-63ccdbb3a9ee>
2013-06-02 19:38:50 +0000

    [advanced_metadata] Add pt_BR, thanks to : manglio
    git-svn-id: http://piwigo.org/svn/extensions/AMetaData@23020 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,38 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['g003_TagId'] = 'Metadados';
+$lang['g003_1_picture_in_caddie'] = '1 fotografia à carregar';
+$lang['amd_title_page'] = 'Gerenciamento Avançado de Metadados';
+$lang['g003_add_a_group'] = 'Adicionar um grupo de metadados';
+$lang['g003_add_a_new_md'] = 'Adicionar novo metadado';
+$lang['g003_add_delete_tags'] = 'Adicionar/remover metadados';
+$lang['g003_add_metadata'] = 'Adicionar um metadado';
+$lang['g003_adding_a_group'] = 'Adicionar um grupo de metadados';
+$lang['g003_advanced_mode'] = 'Avançado';
+$lang['g003_add_a_rule'] = 'Adicionar Regra';
+$lang['g003_add'] = 'Adicionar';
+$lang['g003_Value'] = 'Valor';
+$lang['g003_TagLabel'] = 'Nome';
+$lang['g003_Pct'] = '%';
+$lang['g003_NumOfImage'] = 'Imagens';
+?>
